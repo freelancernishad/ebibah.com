a51c1728115c6236b2ab4b98a48280509eb24d05
freelancernishad <freelancernishad123@gmail.com>
2024-11-12 22:34:56 +0600

    update

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -118,7 +118,7 @@ function profile_matches($type = '', $limit = null)
     $query = User::query();
 
 
-   return $criteriaMatches = getCriteriaMatches($user->id, 246);
+   return $criteriaMatches = getCriteriaMatches($user->id, 286);
     // Determine gender to match against based on authenticated user's gender
     if ($user->gender === 'Male') {
         // If authenticated user is Male, show Female users
